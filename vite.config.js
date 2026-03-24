f95fadcd8b2a10fd8f1a199cbbdb117fa1b2d4cb
Arko-Sengupta <arkosengupta700@gmail.com>
2026-03-25 01:28:04 +0530

    Fixed Deployment Fixes

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true,
+    proxy: {
+      "/api": {
+        target: "https://insta-motion-backend.vercel.app",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
 });
